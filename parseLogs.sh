f9fe7d82bf9f2ec15c779418aa5d47b80223c0e0
Rob <rob@widgetninja.net>
2018-03-04 18:52:12 -0600

    fixed automations

--- parseLogs.sh
+++ parseLogs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git config --global user.email "rob@widgetninja.net"
 git config --global user.name "Rob"
-tail -f *log | grep -v -E 'assert|videostream|Traceback|site-packages|unparsed'
+tail -f -n10000 *log | grep -v -E 'assert|videostream|Traceback|site-packages|unparsed'
